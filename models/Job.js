3beab460a86eb780226b829874fa3c28fb7ea9c9
dcartist <pbannerman13@gmail.com>
2021-12-21 23:32:19 -0500

    using pandas for owners

---
+++ models/Job.js
@@ -0,0 +1,40 @@
+var mongoose = require("mongoose");
+var Schema = mongoose.Schema;
+var Job = new Schema({
+  jobId: { type: String },
+  job__: { type: String },
+  borough: { type: String },
+  street_name: { type: String },
+  city: { type: String },
+  state: { type: String },
+  zip: { type: String },
+  lot: { type: String },
+  job_type: { type: String },
+  job_status: { type: String },
+  job_status_descrp: { type: String },
+  landmarked: { type: String },
+  plumbing: { type: String },
+  propType: { type: String },
+  adult_estab: { type: String },
+  other: { type: String },
+  paid: { type: String },
+  fully_paid: { type: String },
+  approved: { type: String },
+  fully_permitted: { type: String },
+  initial_cost: { type: String },
+  total_est__fee: { type: String },
+  fee_status: { type: String },
+  enlargement_sq_footage: { type: String },
+  proposed_height: { type: String },
+  proposed_dwelling_units: { type: String },
+  zoning_dist1: { type: String },
+  propNum: { type: String },
+  street_name: { type: String },
+  jobDescr: { type: String },
+  signoff_date: { type: String },
+  building_class: { type: String },
+});
+
+
+let job = mongoose.model('Job', Job)
+module.exports = job
